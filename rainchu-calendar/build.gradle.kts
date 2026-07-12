622e61924e671693602c0190b07c121b6b6461a3
rohit pal <rohitpal786420@outlook.com>
2026-07-12 17:44:47 +0530

    Add consumer-rules.pro

--- rainchu-calendar/build.gradle.kts
+++ rainchu-calendar/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 24
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
+      //  consumerProguardFiles("consumer-rules.pro")
     }
 
     buildFeatures {
